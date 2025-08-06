ca549ece0a4ddf1495c2ad0f3174bbe142e9c237
Ester881 <estercamposdasilva7@gmail.com>
2025-08-06 11:29:14 -0300

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,10 @@
+nome = input("digite seu nome: ")
+idade= int(input( "digite sua idade: ")
+
+frasedoseudia = input(" digite uma frase do dia :") 
+A = int(input(" digite um numero :"))
+B = int(input("digite outro numero :"))
+soma = A + B 
+
+print( nome , idade , frasedodia, )
+print( A , B ,soma ) 
